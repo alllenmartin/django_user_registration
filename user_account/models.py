a237636417bdcb4acf3672c72d38f721fc142041
alllenmartin <martinallen722@gmail.com>
2022-09-08 17:18:11 +0300

    user account

--- user_account/models.py
+++ user_account/models.py
@@ -8,13 +8,12 @@ class User(AbstractUser):
     username = models.CharField(max_length=50, blank=True, null=True, unique=True)
     email = models.EmailField('email address', unique=True)
     first_name = models.CharField(max_length=50)
-    middle_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',message="Phone number must be entered in the format:'+999999999'.Up to 15 digits allowed.")
     phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)  # Validators should be a list
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['email', 'username', 'first_name', 'last_name', 'phone_number']
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name', 'phone_number']
 
 class UserProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, related_name='profile')
